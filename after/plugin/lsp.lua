8ff33cc75b70a4040765ca4920f8fc30cc6e72ad
José Gregorio Materán <josemateran1998@gmail.com>
2023-02-11 14:19:20 -0400

    feat: lsp moved to its own file

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,69 @@
+require('neodev').setup()
+
+require('mason').setup()
+
+local mason_lspconfig = require('mason-lspconfig')
+
+mason_lspconfig.setup({
+  ensure_installed = {
+    'sumneko_lua',
+    'tsserver',
+    'gopls'
+  }
+})
+
+local on_attach = function (client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local bufopts = {
+    noremap = true,
+    silent = true,
+    buffer = bufnr
+  }
+
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+end
+
+local lsp_flags = {
+  debounce_text_change = 150
+}
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+mason_lspconfig.setup_handlers {
+  function (server_name)
+    require('lspconfig')[server_name].setup({
+      on_attach = on_attach,
+      flags = lsp_flags,
+      capabilities = capabilities
+    })
+  end
+}
+
+local cmp = require('cmp')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }
+  }, {
+    { name = 'buffer' },
+  })
+})
+
