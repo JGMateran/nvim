4443998962be3d2c52ca25cd66b20e961135ba61
José Gregorio Materán <josemateran1998@gmail.com>
2023-02-11 13:39:09 -0400

    feat: auto install added

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
+local is_bootstrap = false
+
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
   vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
   vim.cmd [[packadd packer.nvim]]
 end
@@ -82,6 +85,10 @@ require('packer').startup(function(use)
       require('nvim-surround').setup()
     end
   }
+
+  if is_bootstrap then
+    require('packer').sync()
+  end
 end)
 
 require('neoscroll').setup()
