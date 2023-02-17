14bdf5193db41a06222f620bc2431ec67167e686
José Gregorio Materán <josemateran1998@gmail.com>
2023-02-17 01:22:17 -0400

    feat: now the linter rules have my preferences

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -1,15 +1,15 @@
-local neogit = require("neogit")
+local neogit = require 'neogit'
 
-neogit.setup({
-	integrations = {
-		diffview = true,
-	},
-})
+neogit.setup {
+  integrations = {
+    diffview = true,
+  },
+}
 
-vim.keymap.set("n", "<leader>gg", function()
-	neogit.open()
+vim.keymap.set('n', '<leader>gg', function()
+  neogit.open()
 end, { noremap = true })
 
-vim.keymap.set("n", "<leader>gc", function()
-	neogit.open({ "commit" })
+vim.keymap.set('n', '<leader>gc', function()
+  neogit.open { 'commit' }
 end, { noremap = true })
