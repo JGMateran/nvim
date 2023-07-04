18689e99c53a2578ce0863b44787149c28bb0b84
José Gregorio Materán <josemateran1998@gmail.com>
2023-07-04 19:22:33 -0400

    feat: rfcc.nvim removed

--- lua/plugins/coding.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- "huggingface/hfcc.nvim",
-  -- opts = {
-  --   api_token = "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",
-  --   accept_keymap = "<C-a>",
-  --   dismiss_keymap = "<C-d>",
-  -- },
-}
