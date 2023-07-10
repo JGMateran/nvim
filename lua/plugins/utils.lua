e75e724c7a6cac5fbc5010b8e83405871c3882ed
José Gregorio Materán <josemateran1998@gmail.com>
2023-07-09 23:45:44 -0400

    feat: trouble plugin added

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -5,6 +5,19 @@ return {
       { "<leader>u", vim.cmd.UndotreeToggle },
     },
   },
+
+  {
+    "folke/trouble.nvim",
+    keys = {
+      { "<leader>xx", "<cmd>TroubleToggle<cr>" },
+      { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
+      { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
+      { "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
+      { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
+      { "<leader>xR", "<cmd>TroubleToggle lsp_references<cr>" },
+    },
+  },
+  {
     "kylechui/nvim-surround",
     version = "*",
     event = "VeryLazy",
