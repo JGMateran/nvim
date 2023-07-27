e77ae224e5748619474bde07353c3f6d9d33b586
José Gregorio Materán <josemateran1998@gmail.com>
2023-07-26 22:34:18 -0400

    feat: linting and formatting for python

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -16,6 +16,8 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.eslint_d,
+        null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.formatting.black,
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.formatting.stylua.with({
