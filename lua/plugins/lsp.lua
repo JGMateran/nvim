673cac6ebbff6879bccf46b595b76594cf406909
José Gregorio Materán <josemateran1998@gmail.com>
2023-08-15 02:11:54 -0400

    feat: remove scrollbar from suggestions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -138,8 +138,12 @@ return {
           end,
         },
         window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
+          completion = cmp.config.window.bordered({
+            scrollbar = false,
+          }),
+          documentation = cmp.config.window.bordered({
+            scrollbar = false,
+          }),
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
