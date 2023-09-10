9a1de8efe5deb3c92a3bf6aa2f97633b75420634
José Gregorio Materán <josemateran1998@gmail.com>
2023-09-10 15:18:37 -0400

    feat: keymaps for incremental selection added

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -40,6 +40,15 @@ return {
       "rust",
       "jsdoc",
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<c-space>",
+        node_incremental = "<c-space>",
+        scope_incremental = "<c-s>",
+        node_decremental = "<M-space>",
+      },
+    },
     textobjects = {
       select = {
         enable = true,
