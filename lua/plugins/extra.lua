7dfead72b7a27c819c701138a7bb7b8d892af38b
José Gregorio Materán <josemateran1998@gmail.com>
2023-11-12 22:41:42 -0400

    feat: splitjoin plugin added

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -70,4 +70,24 @@ return {
   {
     "tpope/vim-sleuth",
   },
+  {
+    "bennypowers/splitjoin.nvim",
+    lazy = true,
+    keys = {
+      {
+        "gj",
+        function()
+          require("splitjoin").join()
+        end,
+        desc = "Join the object under cursor",
+      },
+      {
+        "g,",
+        function()
+          require("splitjoin").split()
+        end,
+        desc = "Split the object under cursor",
+      },
+    },
+  },
 }
