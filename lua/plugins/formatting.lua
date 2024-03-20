b8b7b593d37000d6399e0c7d64f9145dba7657bc
José Gregorio Materán <josemateran1998@gmail.com>
2024-03-20 11:29:39 -0400

    feat: using native format on save

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -37,25 +37,18 @@ return {
   {
     "stevearc/conform.nvim",
     enabled = true,
-    config = function()
-      local conform = require("conform")
-
-      conform.setup({
-        formatters_by_ft = {
-          lua = { "stylua" },
-          javascript = { { "eslint_d", "eslint" } },
-          typescript = { { "eslint_d", "eslint" } },
-          javascriptreact = { { "eslint_d", "eslint" } },
-          typescriptreact = { { "eslint_d", "eslint" } },
-        },
-      })
-
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.lua" },
-        callback = function(args)
-          require("conform").format({ bufnr = args.buf })
-        end,
-      })
-    end,
+    opts = {
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { { "eslint_d", "eslint" } },
+        typescript = { { "eslint_d", "eslint" } },
+        javascriptreact = { { "eslint_d", "eslint" } },
+        typescriptreact = { { "eslint_d", "eslint" } },
+      },
+    },
   },
 }
