52c94e74b7359cc7f6cd04b312f045aa409d3159
José Gregorio Materán <josemateran1998@gmail.com>
2024-03-25 16:49:58 -0400

    feat: remove null-ls

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,42 +1,6 @@
 return {
   {
-    "mhartington/formatter.nvim",
-    enabled = false,
-    config = function()
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
-
-      require("formatter").setup({
-        logging = true,
-        log_level = vim.log.levels.WARN,
-        filetype = {
-          javascript = {
-            require("formatter.filetypes.javascript").eslint_d,
-          },
-          typescript = {
-            require("formatter.filetypes.typescript").eslint_d,
-          },
-          javascriptreact = {
-            require("formatter.filetypes.javascriptreact").eslint_d,
-          },
-          typescriptreact = {
-            require("formatter.filetypes.typescriptreact").eslint_d,
-          },
-          lua = {
-            require("formatter.filetypes.lua").stylua,
-          },
-        },
-      })
-
-      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.lua" },
-        group = augroup,
-        command = "FormatWrite",
-      })
-    end,
-  },
-  {
     "stevearc/conform.nvim",
-    enabled = true,
     opts = {
       format_on_save = {
         timeout_ms = 500,
--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,7 +1,6 @@
 return {
   {
     "mfussenegger/nvim-lint",
-    enabled = true,
     config = function()
       local augroup = vim.api.nvim_create_augroup("LspLinting", { clear = true })
 
@@ -17,61 +16,6 @@ return {
         pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
         callback = function()
           require("lint").try_lint()
-        end,
-      })
-    end,
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    enabled = false,
-    config = function()
-      local null_ls = require("null-ls")
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-      local format_on_save = function(bufnr)
-        vim.lsp.buf.format({
-          bufnr = bufnr,
-          filter = function(client)
-            return client.name == "null-ls"
-          end,
-        })
-      end
-
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.eslint_d,
-          -- null_ls.builtins.diagnostics.flake8,
-          -- null_ls.builtins.formatting.black,
-          null_ls.builtins.diagnostics.eslint_d.with({
-            condition = function(utils)
-              return utils.root_has_file({
-                ".eslintrc.js",
-                ".eslintrc.cjs",
-                ".eslintrc.json",
-              })
-            end,
-          }),
-          -- null_ls.builtins.code_actions.gitsigns,
-          null_ls.builtins.formatting.stylua.with({
-            extra_args = {
-              "--indent-type",
-              "Spaces",
-              "--indent-width",
-              "2",
-            },
-          }),
-        },
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                format_on_save(bufnr)
-              end,
-            })
-          end
         end,
       })
     end,
