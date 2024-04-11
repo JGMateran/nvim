8cef0496bbc69bd82faae01bc633c32d579bbd61
José Gregorio Materán <josemateran1998@gmail.com>
2024-04-11 14:32:12 -0400

    feat: use custom format on save

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -2,10 +2,10 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      -- format_on_save = {
+      --   timeout_ms = 500,
+      --   lsp_fallback = true,
+      -- },
       formatters_by_ft = {
         lua = { "stylua" },
         javascript = { { "eslint_d", "eslint" } },
@@ -14,5 +14,22 @@ return {
         typescriptreact = { { "eslint_d", "eslint" } },
       },
     },
+    config = function(_, opts)
+      local conform = require("conform")
+
+      conform.setup(opts)
+
+      local group = vim.api.nvim_create_augroup("Conform", { clear = true })
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.lua" },
+        group = group,
+        callback = function(args)
+          conform.format({
+            bufnr = args.buf,
+          })
+        end,
+      })
+    end,
   },
 }
