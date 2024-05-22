73235396559d9388be8f5dcc21962fa08be78fec
José Gregorio Materán <josemateran1998@gmail.com>
2024-05-21 20:18:30 -0400

    feat: add which key

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -107,4 +107,13 @@ return {
       })
     end,
   },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {},
+  },
 }
