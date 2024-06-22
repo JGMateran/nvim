c81ee7d93711b5bc2075a2572c4588ebf63337db
José Gregorio Materán <josemateran1998@gmail.com>
2024-06-21 22:19:20 -0400

    feat: linting if config file exists

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,17 +1,43 @@
+local function root_file(files, dirname)
+  local found = vim.fs.find(files, { upward = true, path = dirname })[1]
+
+  if found then
+    return true
+  end
+
+  return false
+end
+
 return {
   {
     "mfussenegger/nvim-lint",
-    config = function()
-      local augroup = vim.api.nvim_create_augroup("LspLinting", { clear = true })
+    opts = {
+      linters_by_ft = {
+        javascript = { "eslint_d", "biomejs" },
+        typescript = { "eslint_d", "biomejs" },
+        javascriptreact = { "eslint_d", "biomejs" },
+        typescriptreact = { "eslint_d", "biomejs" },
+      },
+      linters = {
+        eslint_d = {
+          condition = function(ctx)
+            return root_file({ ".eslintrc.js", ".eslintrc.json" }, ctx.filename)
+          end,
+        },
+        biomejs = {
+          condition = function(ctx)
+            return root_file({ "biome.jsonc", "biome.json" }, ctx.filename)
+          end,
+        },
+      },
+    },
+    config = function(_, opts)
+      local M = {}
+      local lint = require("lint")
 
-      require("lint").linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-      }
+      lint.linters_by_ft = opts.linters_by_ft
 
-      local function debounce(ms, fn)
+      function M.debounce(ms, fn)
         local timer = vim.uv.new_timer()
         return function(...)
           local argv = { ... }
@@ -22,14 +48,30 @@ return {
         end
       end
 
-      local function lint()
-        require("lint").try_lint()
+      function M.lint()
+        local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+
+        local ctx = {}
+
+        ctx.filename = vim.api.nvim_buf_get_name(0)
+        ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
+
+        local filtered_names = vim.tbl_filter(function(name)
+          local linter = opts.linters[name]
+          if not linter then
+            print("Linter not found: " .. name)
+          end
+
+          return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
+        end, names)
+
+        lint.try_lint(filtered_names)
       end
 
       vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        group = augroup,
+        group = vim.api.nvim_create_augroup("LspLinting", { clear = true }),
         pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
-        callback = debounce(500, lint),
+        callback = M.debounce(500, M.lint),
       })
     end,
   },
