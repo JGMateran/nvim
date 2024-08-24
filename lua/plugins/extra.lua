db1daa4b81a417a9742e69d86e3704e8f9406bf8
José Gregorio Materán <josemateran1998@gmail.com>
2024-08-24 02:44:07 -0400

    feat: remove comments

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -131,9 +131,6 @@ return {
       highlight = {
         timer = 150,
       },
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
     },
   },
 }
