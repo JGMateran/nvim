0267ca2447498dabb6fcfcbaf240cc99404e76f1
José Gregorio Materán <josemateran1998@gmail.com>
2024-09-07 16:16:10 -0400

    feat use stop_after_first

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -4,10 +4,10 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { { "biome-check", "eslint_d", "eslint" } },
-        typescript = { { "biome-check", "eslint_d", "eslint" } },
-        javascriptreact = { { "biome-check", "eslint_d", "eslint" } },
-        typescriptreact = { { "biome-check", "eslint_d", "eslint" } },
+        javascript = { "biome-check", "eslint_d", "eslint", stop_after_first = true },
+        typescript = { "biome-check", "eslint_d", "eslint", stop_after_first = true },
+        javascriptreact = { "biome-check", "eslint_d", "eslint", stop_after_first = true },
+        typescriptreact = { "biome-check", "eslint_d", "eslint", stop_after_first = true },
       },
     },
     config = function(_, opts)
@@ -18,7 +18,7 @@ return {
       local group = vim.api.nvim_create_augroup("Conform", { clear = true })
 
       vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.lua" },
+        pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.lua", "*.mjs" },
         group = group,
         callback = function(args)
           conform.format({
