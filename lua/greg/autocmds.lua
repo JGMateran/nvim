1ac4dcd51559ab577767e47d2127a3201a7fe719
José Gregorio Materán <josemateran1998@gmail.com>
2024-12-21 14:38:42 -0400

    feat: change lsp loading icon

--- lua/greg/autocmds.lua
+++ lua/greg/autocmds.lua
@@ -59,7 +59,8 @@ vim.api.nvim_create_autocmd("LspProgress", {
       id = "lsp_progress",
       title = "LSP Progress",
       opts = function(notif)
-        notif.icon = ev.data.params.value.kind == "end" and ""
+        notif.icon = ev.data.params.value.kind == "end" and " "
+          or " "
           or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
       end,
     })
