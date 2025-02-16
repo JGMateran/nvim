cc0699fb2b5e2df5ac3677c46256b47c2cb9f40a
José Gregorio Materán <josemateran1998@gmail.com>
2025-02-16 13:50:39 -0400

    feat: add avante.nvim

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,66 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "gemini",
+      behaviour = {},
+      hints = {
+        enabled = false,
+      },
+      file_selector = {
+        provider = "snacks",
+        height = 2,
+      },
+      windows = {
+        sidebar_header = {
+          enabled = false,
+        },
+        height = 40,
+        input = {
+          prefix = " ",
+          height = 3,
+        },
+        ask = {
+          floating = true,
+        },
+        position = "right",
+      },
+    },
+    build = "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "echasnovski/mini.pick",
+      "nvim-telescope/telescope.nvim",
+      "hrsh7th/nvim-cmp",
+      "ibhagwan/fzf-lua",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "Avante" },
+        },
+        ft = { "Avante" },
+      },
+    },
+  },
+}
