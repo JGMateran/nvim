7e9ddb43b312acbe2c38261942d577b3773e2ffb
José Gregorio Materán <josemateran1998@gmail.com>
2025-04-18 21:43:59 -0400

    feat: move lsp autocmd to lsp.lua

--- lua/greg/autocmds.lua
+++ lua/greg/autocmds.lua
@@ -6,51 +6,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(e)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[e.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = e.buf
-      vim.keymap.set(mode, l, r, opts)
-    end
-
-    map("n", "gD", vim.lsp.buf.declaration, { desc = "Show declaration" })
-    map("n", "gd", "<cmd>Telescope lsp_definitions<cr>", { desc = "Show definitions" })
-    map("n", "K", function()
-      vim.lsp.buf.hover({
-        border = "rounded",
-        max_width = 80,
-      })
-    end, { desc = "Display detailed information about the symbol under the cursor" })
-    map("n", "gi", "<cmd>Telescope lsp_implementations<cr>", { desc = "Show implementations" })
-    map("n", "<C-k>", function()
-      vim.lsp.buf.signature_help({
-        border = "rounded",
-        max_width = 80,
-      })
-    end, { desc = "Show signature help" })
-    map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, { desc = "Add workspace folder" })
-    map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, { desc = "Remove workspace folder" })
-    map("n", "<space>wl", function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, { desc = "List workspace folders" })
-    map("n", "<space>D", "<cmd>Telescope lsp_type_definitions<cr>", { desc = "Show type definitions" })
-    map("n", "<space>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
-    map({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-    map("n", "gr", vim.lsp.buf.references, { desc = "Show references" })
-    map("n", "<space>f", function()
-      vim.lsp.buf.format({ async = true })
-    end, { desc = "Format document" })
-
-    map("n", "<space>gs", "<cmd>Telescope git_status<cr>", { desc = "Git Status" })
-    map("n", "<space>gb", "<cmd>Telescope git_branches<cr>", { desc = "Git Branches" })
-  end,
-})
-
 -- resize splits if window got resized
 vim.api.nvim_create_autocmd({ "VimResized" }, {
   group = vim.api.nvim_create_augroup("ResizeSplit", { clear = true }),
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -90,22 +90,52 @@ return {
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.diagnostic.config({
-        float = {
-          border = "rounded",
-          max_width = 80,
-        },
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = " ",
-            [vim.diagnostic.severity.WARN] = " ",
-            [vim.diagnostic.severity.INFO] = " ",
-            [vim.diagnostic.severity.HINT] = "󰌵 ",
-          },
-        },
-      })
-
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(e)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[e.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = e.buf
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          map("n", "gD", vim.lsp.buf.declaration, { desc = "Show declaration" })
+          map("n", "gd", "<cmd>Telescope lsp_definitions<cr>", { desc = "Show definitions" })
+          map("n", "K", function()
+            vim.lsp.buf.hover({
+              border = "rounded",
+              max_width = 80,
+            })
+          end, { desc = "Display detailed information about the symbol under the cursor" })
+          map("n", "gi", "<cmd>Telescope lsp_implementations<cr>", { desc = "Show implementations" })
+          map("n", "<C-k>", function()
+            vim.lsp.buf.signature_help({
+              border = "rounded",
+              max_width = 80,
+            })
+          end, { desc = "Show signature help" })
+          map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, { desc = "Add workspace folder" })
+          map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, { desc = "Remove workspace folder" })
+          map("n", "<space>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, { desc = "List workspace folders" })
+          map("n", "<space>D", "<cmd>Telescope lsp_type_definitions<cr>", { desc = "Show type definitions" })
+          map("n", "<space>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+          map({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+          -- map("n", "gr", vim.lsp.buf.references, { desc = "Show references" })
+          map("n", "<space>f", function()
+            vim.lsp.buf.format({ async = true })
+          end, { desc = "Format document" })
+
+          map("n", "<space>gs", "<cmd>Telescope git_status<cr>", { desc = "Git Status" })
+          map("n", "<space>gb", "<cmd>Telescope git_branches<cr>", { desc = "Git Branches" })
+        end,
+      })
 
       require("mason-tool-installer").setup({
         ensure_installed = vim.tbl_keys(servers),
@@ -121,6 +151,21 @@ return {
 
             lspconfig[server_name].setup(server)
           end,
+        },
+      })
+
+      vim.diagnostic.config({
+        float = {
+          border = "rounded",
+          max_width = 80,
+        },
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+            [vim.diagnostic.severity.HINT] = "󰌵 ",
+          },
         },
       })
     end,
