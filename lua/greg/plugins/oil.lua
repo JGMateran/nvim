9e350a47869fb62c992277f623e4ae15d376d717
José Gregorio Materán <josemateran1998@gmail.com>
2026-04-11 11:33:15 -0400

    feat(navigation): optimize oil.nvim and clean up unused IA plugins

--- lua/greg/plugins/oil.lua
+++ lua/greg/plugins/oil.lua
@@ -1,20 +1,47 @@
 vim.pack.add({
   "https://github.com/stevearc/oil.nvim",
-  "https://github.com/nvim-tree/nvim-web-devicons",
 })
 
 require("oil").setup({
+  default_file_explorer = true,
+  columns = {
+    "icon",
+    "size",
+  },
+  buf_options = {
+    buflisted = false,
+    bufhidden = "hide",
+  },
   win_options = {
-    number = false,
-    relativenumber = false,
-    cursorline = true,
+    wrap = false,
+    signcolumn = "no",
+    cursorcolumn = false,
+    foldcolumn = "0",
+    spell = false,
+    list = false,
+    concealcursor = "nvic",
+    conceallevel = 3,
+  },
+  delete_to_trash = true,
+  skip_confirm_for_simple_edits = true,
+  view_options = {
+    show_hidden = true,
+    is_hidden_file = function()
+      return false
+    end,
+    is_always_hidden_file = function()
+      return false
+    end,
   },
   float = {
-    preview_split = "right",
     padding = 2,
-    max_width = 70,
-    max_height = 70,
+    max_width = 0,
+    max_height = 0,
+    border = "rounded",
+    win_options = {
+      winblend = 0,
+    },
   },
 })
 
-vim.keymap.set("n", "-", "<cmd>Oil --float<cr>", { desc = "Open Oil" })
+vim.keymap.set("n", "-", "<cmd>Oil --float<cr>", { desc = "Open parent directory in Oil" })
