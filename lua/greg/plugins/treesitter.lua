675146ae33c86cb1ad3ec7041501e7deea789cdd
José Gregorio Materán <josemateran1998@gmail.com>
2026-04-18 00:31:31 -0400

    feat: migrate treesitter for neovim 0.12+ compatibility
    - Update nvim-treesitter and nvim-treesitter-textobjects to main branch.
    - Rewrite configuration for native highlighting and folding.
    - Disable incompatible splitjoin plugin.

--- lua/greg/plugins/treesitter.lua
+++ lua/greg/plugins/treesitter.lua
@@ -3,57 +3,48 @@ vim.pack.add({
   "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
 })
 
-require("nvim-treesitter.configs").setup({
-  sync_install = false,
-  auto_install = true,
-  ignore_install = {},
-  modules = {},
-  highlight = {
-    enable = true,
-  },
-  ensure_installed = {
-    "vue",
-    "astro",
-    "bash",
-    "css",
-    "html",
-    "javascript",
-    "json",
-    "lua",
-    "markdown",
-    "prisma",
-    "regex",
-    "tsx",
-    "typescript",
-    "vim",
-    "python",
-    "jsdoc",
-    "php",
-    "ron",
-    "rust",
-    "toml",
-    "vimdoc",
-    "sql",
-    "http",
-    "go",
-    "gomod",
-    "gosum",
-    "gowork",
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ["aa"] = "@parameter.outer",
-        ["ia"] = "@parameter.inner",
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-      },
-    },
-  },
+require("nvim-treesitter").setup({})
+
+local parsers = {
+  "vue",
+  "astro",
+  "bash",
+  "css",
+  "html",
+  "javascript",
+  "json",
+  "lua",
+  "markdown",
+  "prisma",
+  "regex",
+  "tsx",
+  "typescript",
+  "vim",
+  "python",
+  "jsdoc",
+  "php",
+  "ron",
+  "rust",
+  "toml",
+  "vimdoc",
+  "sql",
+  "http",
+  "go",
+  "gomod",
+  "gosum",
+  "gowork",
+}
+
+require("nvim-treesitter").install(parsers)
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+
+    if lang then
+      pcall(vim.treesitter.start, args.buf, lang)
+    end
+  end,
 })
 
 vim.filetype.add({
